0cee5d0c28637a0f86e4ffaebb6dcd63b5a33d26
mlritchie <ritchie.mike@gmail.com>
2021-06-07 22:06:23 -0400

    v2.1.1
    Fixed bug with Number of Hours from Current Time preference not showing.
    Refactored caching functionality to work with Search range preferences. 
    Caching may have been used before that didn't contain all events. Reworded instructions a bit to be clearer.

--- Driver/GCal_Switch.groovy
+++ Driver/GCal_Switch.groovy
@@ -1,4 +1,4 @@
-def driverVersion() { return "2.2.0" }
+def driverVersion() { return "2.2.1" }
 /**
  *  GCal Switch Driver
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Driver/GCal_Switch.groovy
