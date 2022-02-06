1348513b03122ba3391844589ef2ac24634f52e2
mlritchie <ritchie.mike@gmail.com>
2022-02-06 17:24:19 -0500

    v3.1.1
    Fixed issue with trigger notifications sending the wrong date when offset is utilized
    Added details about the next scheduled item at the top of the trigger app

--- Apps/GCal_Search.groovy
+++ Apps/GCal_Search.groovy
@@ -1,4 +1,4 @@
-def appVersion() { return "3.1.0" }
+def appVersion() { return "3.1.1" }
 /**
  *  GCal Search
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Apps/GCal_Search.groovy
