7ba021445a0872f9ad0668520cf57dbdaa43e679
mlritchie <ritchie.mike@gmail.com>
2023-01-25 08:52:51 -0500

    v3.5.6
    Fixed NullPointerException error when creating new search trigger or editing an existing trigger.

--- Apps/GCal_Search.groovy
+++ Apps/GCal_Search.groovy
@@ -1,4 +1,4 @@
-def appVersion() { return "3.5.5" }
+def appVersion() { return "3.5.6" }
 /**
  *  GCal Search
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Apps/GCal_Search.groovy
