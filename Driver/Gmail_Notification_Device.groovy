3baa22986a02297e2c644a9fab2a23cb73edc355
mlritchie <ritchie.mike@gmail.com>
2023-03-16 13:18:35 -0400

    v4.0.0
    Re-release of the Gmail Search and Notification Device with new version number.
    - Added ability to dynamically set the recipient and subject of a Gmail notification within the notification message
    - Added the ability to specify a file within the Gmail notification message to be included in the email as an attachment
    - When setting up new Gmail Search triggers, the search string is no longer required

--- Driver/Gmail_Notification_Device.groovy
+++ Driver/Gmail_Notification_Device.groovy
@@ -1,4 +1,4 @@
-def driverVersion() { return "3.5.7" }
+def driverVersion() { return "4.0.0" }
 /**
  *  Gmail Notification Device Driver
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Driver/Gmail_Notification_Device.groovy
