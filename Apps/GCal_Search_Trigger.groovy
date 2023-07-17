479ff45605e4574ab23c84b1cf3968a6c34e8e13
mlritchie <ritchie.mike@gmail.com>
2023-07-17 13:37:11 -0400

    v4.4.0
    Fixed bug to address situation when hub and target Google Calendar are set to different time zones.
    Added 'Debug authentication' setting to the parent GCal Search app that controls debug logging of the authentication to the Google APIs since this can contain sensitive information if shared. This defaults to false.
    Child GCal Search Trigger now passes its app label to the parent GCal Search app so you can distinguish debug logging if multiple search triggers are scheduled at the same time.
    If a GCal Search Trigger has a child switch, the Last Refreshed text at the very top will be a clickable link so you can quickly navigate to that child device.
    A new 'Use offset for start and end timestamps' preference was added to the GCal Switch driver that will control whether the eventStartTime and eventEndTime attributes use the offset timestamps versus the actual event timestamps.

--- Apps/GCal_Search_Trigger.groovy
+++ Apps/GCal_Search_Trigger.groovy
@@ -1,4 +1,4 @@
-def appVersion() { return "4.3.0" }
+def appVersion() { return "4.4.0" }
 /**
  *  GCal Search Trigger Child Application
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Apps/GCal_Search_Trigger.groovy
@@ -50,7 +50,14 @@ def mainPage() {
 				input name: "resumeButton", type: "button", title: "Resume", backgroundColor: "Crimson", textColor: "white", width: 4, submitOnChange: true
 			}
             if (state.refreshed) {
-                paragraph "Last Refreshed:\n${parseDateTime(state.refreshed)}", width: 4
+                def refreshText = parseDateTime(state.refreshed)
+                if (state.installed == true && settings.createChildSwitch) {
+                    def childSwitch = getChildDevice(state.deviceID)
+                    refreshText = "<a href='/device/edit/$childSwitch.id' target='_blank' title='Open Device Page for $childSwitch'>Last Refreshed</a>:\n${refreshText}"
+                } else {
+                    refreshText = "Last Refreshed:\n${refreshText}"
+                }
+                paragraph "${refreshText}", width: 4
             }
             if (!state.isPaused) {
                 input name: "refreshButton", type: "button", title: "Refresh", width: 4, submitOnChange: true
@@ -91,7 +98,7 @@ def mainPage() {
                     if (watchListOptions == "error") {
                         paragraph "${parent.getFormat("warning", "The Google Calendar API has not been enabled in your Google project.  <a href='https://console.cloud.google.com/apis/api/calendar-json.googleapis.com' target='_blank'>Please click here to add it the in Google Console</a>. Then refresh this page.")}"
                     } else {
-                        logDebug("Calendar list = ${watchListOptions}")
+                        //logDebug("Calendar list = ${watchListOptions}")
                         input name: "watchList", title:"Which calendar do you want to search?", type: "enum", required:true, multiple:false, options:watchListOptions, submitOnChange: true
                         input name: "includeAllDay", type: "bool", title: "Include All Day Events?", defaultValue: true, required: false
                         input name: "searchField", type: "enum", title: "Calendar field to search", required: true, defaultValue: "title", options:["title","location"]
@@ -695,7 +702,7 @@ def getNextEvents() {
         offsetEnd = settings.offsetEnd.toInteger()
         offsetEnd = offsetEnd * 60 * 1000
     }
-    def items = parent.getNextEvents(settings.watchList, settings.GoogleMatching, search, endTimePreference, offsetEnd, dateFormat)
+    def items = parent.getNextEvents(settings.watchList, settings.GoogleMatching, search, endTimePreference, offsetEnd, dateFormat, app.label)
     logMsg.push("getNextEvents - BEFORE search: ${search}, items: ${items} AFTER ")
     
     def foundMatch = false
@@ -793,7 +800,7 @@ def getNextEvents() {
                 def tempStartTime = item.scheduleStartTime.getTime()
                 tempStartTime = tempStartTime + (offsetStart * 60 * 1000)
                 item.scheduleStartTime.setTime(tempStartTime)
-                logMsg.push("Event start offset: ${settings.offsetStart}, adjusting time from ${origStartTime} to ${item.scheduleStartTime}")
+                logMsg.push("\nEvent start offset: ${settings.offsetStart}, adjusting time from ${origStartTime} to ${item.scheduleStartTime}")
             }
             
             item.scheduleEndTime = new Date(item.eventEndTime.getTime())
@@ -805,12 +812,12 @@ def getNextEvents() {
                 tempEndTime = item.scheduleEndTime.getTime()
                 tempEndTime = tempEndTime + offsetEnd
                 item.scheduleEndTime.setTime(tempEndTime)
-                logMsg.push("Event end offset: ${settings.offsetEnd}, adjusting time from ${origEndTime} to ${item.scheduleEndTime}")
+                logMsg.push("\nEvent end offset: ${settings.offsetEnd}, adjusting time from ${origEndTime} to ${item.scheduleEndTime}")
             }
             items[i] = item
         }
         
-        logMsg.push("foundMatch: ${foundMatch}")
+        logMsg.push("\nfoundMatch: ${foundMatch}")
         if (foundMatch) {
             item  = items[0]
         }
@@ -839,7 +846,7 @@ def getNextTasks() {
     def logMsg = []
     def search = (!settings.search) ? "" : settings.search
     def endTimePreference = (settings.endTimePref == "Number of Hours from Current Time") ? settings.endTimeHours : settings.endTimePref
-    def items = parent.getNextTasks(settings.watchList, search, endTimePreference)
+    def items = parent.getNextTasks(settings.watchList, search, endTimePreference, app.label)
     logMsg.push("getNextTasks - BEFORE search: ${search}, items:\n${items.join("\n")}\nAFTER ")
     
     def sequentialEventOffset = false
@@ -916,7 +923,7 @@ def getNextReminders() {
     def logMsg = []
     def search = (!settings.search) ? "" : settings.search
     def endTimePreference = (settings.endTimePref == "Number of Hours from Current Time") ? settings.endTimeHours : settings.endTimePref
-    def items = parent.getNextReminders(search, endTimePreference)
+    def items = parent.getNextReminders(search, endTimePreference, app.label)
     logMsg.push("getNextReminders - BEFORE search: ${search}, items:\n${items.join("\n")}\nAFTER ")
     
     def completeAllPastDue = true
