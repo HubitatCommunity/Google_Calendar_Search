656331f65d5954223e8d112ee60e7a07f04396a6
mlritchie <ritchie.mike@gmail.com>
2023-08-02 13:33:42 -0400

    v4.4.2
    Fixed a NullPointerException error when searching for Gmail messages

--- Driver/Gmail_Notification_Device.groovy
+++ Driver/Gmail_Notification_Device.groovy
@@ -1,4 +1,4 @@
-def driverVersion() { return "4.4.1" }
+def driverVersion() { return "4.4.2" }
 /**
  *  Gmail Notification Device Driver
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Driver/Gmail_Notification_Device.groovy
