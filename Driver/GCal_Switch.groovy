7c4ccbfdf3090cc3c38c74a75bb2e07e000eb913
mlritchie <ritchie.mike@gmail.com>
2024-02-11 12:04:33 -0500

    v4.6.2
    Fixed issue with Additional Actions Control Switches where switches listed were toggling at the event scheduled event even though the setting was set to false.

--- Driver/GCal_Switch.groovy
+++ Driver/GCal_Switch.groovy
@@ -1,4 +1,4 @@
-def driverVersion() { return "4.6.1" }
+def driverVersion() { return "4.6.2" }
 /**
  *  GCal Switch Driver
  *  https://raw.githubusercontent.com/HubitatCommunity/Google_Calendar_Search/main/Driver/GCal_Switch.groovy
